f47c8feec83682863234eeb384107e871a7de12d
angel210819 <89662810+angel210819@users.noreply.github.com>
2022-08-18 11:06:34 -0300

    primer commit de calculadora

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Boton from './components/btn';
+import Pantalla from './components/screen';
+import BotonClear from './components/btn-clear';
+import { useState } from 'react'
+import { evaluate } from 'mathjs'
+
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input + val);
+  };
+
+  const calculadorResultado = () => {
+    if (input) {
+      setInput(evaluate(input))
+    } else {
+      alert('por favor ingrese valores para realizar los calculos')
+    }
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      
+          <div className='contenedor-calculadora'>
+          <Pantalla 
+            input={input}
+          />
+            <div className='fila'>
+              <Boton manejarClic={agregarInput}>1</Boton>
+              <Boton manejarClic={agregarInput}>2</Boton>
+              <Boton manejarClic={agregarInput}>3</Boton>
+              <Boton manejarClic={agregarInput}>+</Boton>
+            </div>
+            <div className='fila'>
+              <Boton manejarClic={agregarInput}>4</Boton>
+              <Boton manejarClic={agregarInput}>5</Boton>
+              <Boton manejarClic={agregarInput}>6</Boton>
+              <Boton manejarClic={agregarInput}>-</Boton>
+            </div>
+            <div className='fila'>
+              <Boton manejarClic={agregarInput}>7</Boton>
+              <Boton manejarClic={agregarInput}>8</Boton>
+              <Boton manejarClic={agregarInput}>9</Boton>
+              <Boton manejarClic={agregarInput}>*</Boton>
+            </div>
+            <div className='fila'>
+              <Boton manejarClic={calculadorResultado}>=</Boton>
+              <Boton manejarClic={agregarInput}>0</Boton>
+              <Boton manejarClic={agregarInput}>.</Boton>
+              <Boton manejarClic={agregarInput}>/</Boton>
+            </div>
+            <div className='fila'>
+              <BotonClear manejarClear={() => setInput('')}>
+                Clear
+              </BotonClear>
+            </div>
+          </div>
     </div>
   );
 }
---
+++ src/components/screen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../stylesheet/screen.css'
+
+const Pantalla = ({ input }) => (
+  <div className='input'>
+    {input}
+  </div>
+)
+
+export default Pantalla
